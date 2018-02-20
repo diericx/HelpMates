a9045c078a21d4d043c235e5401ddc44406df499
Zac Holland <zacharyholland@gmail.com>
2018-02-19 23:39:24 -0700

    Added Messenging system to session view

--- app/scenes/HelpSession/show.js
+++ app/scenes/HelpSession/show.js
@@ -1,31 +1,72 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Meteor, { createContainer } from 'react-native-meteor';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const styles = EStyleSheet.create({
-  container: {},
+  container: { flex: 1 },
 });
 
-export default class Show extends React.Component {
-  static navigationOptions = {
-    title: 'Session',
-    headerStyle: {
-      backgroundColor: '#cd84f1',
-    },
+class Show extends React.Component {
+  onSend(convoId, messages = []) {
+    const message = messages[0];
+    message.user.name = Meteor.user().profile.name;
+    this.sendMessage(convoId, message);
+  }
 
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontSize: 25,
-      fontWeight: 'bold',
-      fontFamily: 'Milkshake',
-    },
-  };
+  sendMessage(conversationId, message) {
+    Meteor.call('conversations.sendMessage', { conversationId, message }, (err, res) => {
+      // Do whatever you want with the response
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Sent Message!');
+      }
+    });
+  }
+
+  renderChat(conversation) {
+    console.log('Conve: ', conversation);
+    if (conversation) {
+      console.log('conversation is here');
+      return (
+        <GiftedChat
+          messages={conversation.messages}
+          onSend={messages => this.onSend(conversation._id, messages)}
+          user={{
+            _id: Meteor.userId(),
+          }}
+        />
+      );
+    }
+    return <View />;
+  }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text> Help Session </Text>
-      </View>
-    );
+    const { conversation } = this.props;
+    return <View style={styles.container}>{this.renderChat(conversation)}</View>;
   }
 }
+
+export default (container = createContainer((params) => {
+  const convoId = params.navigation.state.params.conversationId;
+  Meteor.subscribe('thisConversation', { id: convoId });
+  return {
+    conversation: Meteor.collection('conversations').findOne(convoId),
+  };
+}, Show));
+
+container.navigationOptions = {
+  title: 'Session',
+  headerStyle: {
+    backgroundColor: '#cd84f1',
+  },
+
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    fontFamily: 'Milkshake',
+  },
+};
