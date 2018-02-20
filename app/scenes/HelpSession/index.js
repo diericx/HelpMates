5c590dd9c594669cd1ec40a8774975483bc8a411
Zac Holland <zacharyholland@gmail.com>
2018-02-20 00:41:39 -0700

    Session requests are almost updating correctly

--- app/scenes/HelpSession/index.js
+++ app/scenes/HelpSession/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Button, SectionList } from 'react-native';
-import Meteor from 'react-native-meteor';
+import Meteor, { createContainer } from 'react-native-meteor';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { List, ListItem } from 'react-native-elements';
 
@@ -12,26 +12,9 @@ const styles = EStyleSheet.create({
 
 const defaultAvatar = 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg';
 
-export default class Index extends React.Component {
-  static navigationOptions = {
-    title: 'My Sessions',
-    headerStyle: {
-      backgroundColor: '#cd84f1',
-    },
-
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontSize: 25,
-      fontWeight: 'bold',
-      fontFamily: 'Milkshake',
-    },
-  };
-
+class Index extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      sessions: null,
-    };
 
     // Get data from server
     this.getSessions();
@@ -53,31 +36,66 @@ export default class Index extends React.Component {
     this.props.navigation.navigate('Show', params);
   }
 
-  renderSessionList() {
-    if (this.state.sessions == null) {
-      // render loading circle
-      return <View />;
-    } else {
-      // render list
-      return (
-        <List containerStyle={{ marginBottom: 20 }}>
-          {this.state.sessions.map((l, i) => (
-            <ListItem
-              onPress={() => this.onItemPress(l)}
-              underlayColor="rgb(245,245,245)"
-              roundAvatar
-              avatar={{ uri: defaultAvatar }}
-              key={i}
-              title={l.name}
-              subtitle={l.subtitle}
-            />
-          ))}
-        </List>
-      );
+  getNameToDisplayForSession(session) {
+    if (Meteor.userId() == session.userId) {
+      var user = Meteor.collection('users').findOne(session.tutorId);
+      if (user) {
+        return user.profile.name;
+      }
+    }
+    var user = Meteor.collection('users').findOne(session.userId);
+    if (user) {
+      return user.profile.name;
     }
   }
 
+  renderSessionList(sessions) {
+    if (sessions == null) {
+      // render loading circle
+      return <View />;
+    }
+    // render list
+    return (
+      <List containerStyle={{ marginBottom: 20 }}>
+        {sessions.map((l, i) => (
+          <ListItem
+            onPress={() => this.onItemPress(l)}
+            underlayColor="rgb(245,245,245)"
+            roundAvatar
+            avatar={{ uri: defaultAvatar }}
+            key={i}
+            title={this.getNameToDisplayForSession(l)}
+            subtitle={l.subtitle}
+          />
+        ))}
+      </List>
+    );
+  }
+
   render() {
-    return <View style={styles.container}>{this.renderSessionList()}</View>;
+    const { sessions } = this.props;
+    console.log('Sessions: ', sessions);
+    return <View style={styles.container}>{this.renderSessionList(sessions)}</View>;
   }
 }
+
+export default (container = createContainer((params) => {
+  Meteor.subscribe('mySessions');
+  return {
+    sessions: Meteor.collection('helpSessions').find(),
+  };
+}, Index));
+
+container.navigationOptions = {
+  title: 'My Sessions',
+  headerStyle: {
+    backgroundColor: '#cd84f1',
+  },
+
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    fontFamily: 'Milkshake',
+  },
+};
