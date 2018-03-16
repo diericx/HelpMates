e9c48fd4d8cbfa8196cb25f1a126e513a6753d9e
Zac Holland <zacharyholland@gmail.com>
2018-03-15 19:40:26 -0600

    App now automatically connects to API running on host machine

--- app/connect.js
+++ app/connect.js
@@ -1,6 +1,16 @@
 import Meteor from 'react-native-meteor';
+import Expo from 'expo';
+
+const { manifest } = Expo.Constants;
+const api = manifest.packagerOpts.dev
+  ? manifest.debuggerHost
+    .split(':')
+    .shift()
+    .concat(':3000/websocket')
+  : 'localhost:3000/websocket';
 
 export default function () {
-  const url = 'ws://10.7.107.240:3000/websocket';
+  console.log(api);
+  const url = `ws://${api}`;
   Meteor.connect(url);
 }
