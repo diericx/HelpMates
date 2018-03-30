9a666ef78cfb398d4d7106aeb213432142bb26ff
Grace <gkorniczky@outlook.com>
2018-03-30 16:57:31 -0600

    admin chat begun

--- app/scenes/Profile/AdminChat/index.js
+++ app/scenes/Profile/AdminChat/index.js
@@ -1,19 +1,67 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import EStyleSheet from 'react-native-extended-stylesheet';
+import { View, Text, AsyncStorage } from 'react-native';
+import { Button } from 'react-native-elements';
+import Meteor, { createContainer } from 'react-native-meteor';
+import { GiftedChat } from 'react-native-gifted-chat';
+import Faker from 'faker';
+import ActivityIndicator from 'app/components/general/ActivityIndicator';
+import { SendMessage } from 'app/Helpers/Meteor';
 
 import styles from './styles';
 
-export default class Index extends React.Component {
-  static navigationOptions = {
-    headerTitle: 'Chat with us!'
-  };
+class Show extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // When a message is sent on client
+  onSend(convoId, messages = []) {
+    const message = messages[0];
+    message.user.name = this.state.name;
+    SendMessage(convoId, message);
+  }
+
+  // Render the chat UI element
+  renderChat(conversation) {
+    if (conversation) {
+      return (
+        <GiftedChat
+          messages={conversation.messages.reverse()}
+          bottomOffset={50}
+          onSend={messages => this.onSend(conversation._id, messages)}
+          user={{
+            _id: this.state.guid,
+          }}
+          renderLoading={() => <ActivityIndicator size="large" marginTop={35} />}
+        />
+      );
+    }
+    return <View />;
+  }
 
   render() {
+    const { conversation } = this.props;
     return (
-      <View>
-        <Text>admin chat texting</Text>
+      <View style={styles.container}>
+        <View style={styles.chat}>{this.renderChat(conversation)}</View>
       </View>
     );
   }
 }
+
+const container = createContainer((params) => {
+  // const { course } = params.navigation.state.params;
+  // Meteor.subscribe('getConversation', { id: course.conversationId });
+  return {
+    conversation: Meteor.collection('conversations').findOne(),
+  };
+}, Show);
+
+container.navigationOptions = ({ navigation }) => {
+  const { state: { params = {} } } = navigation;
+  return {
+    headerTitle: params.title || 'Admin Chat',
+  };
+};
+
+export default container;
