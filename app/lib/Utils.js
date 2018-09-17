6a3b0729f0db0dce833b34be9d6af9aa4b26e5da
Zac Holland <zacharyholland@gmail.com>
2018-09-17 12:52:23 -0600

    People can block user's and all the content from that user goes away

--- app/lib/Utils.js
+++ app/lib/Utils.js
@@ -1,4 +1,8 @@
-// validates that the given arguments exist, if not prints given error message
+/**
+ * validates that the given arguments exist, if not prints given error message
+ * @param {String} errorMessage - message to pring on validation error
+ * @param  {...any} args - values to validate
+ */
 export function validate(errorMessage, ...args) {
   args.forEach((arg, index) => {
     if (!arg) {
