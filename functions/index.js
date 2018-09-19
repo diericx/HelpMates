dfe66676e5ae80ad7eb48f9b09b124a5a4163ce2
Zac Holland <zacharyholland@gmail.com>
2018-09-19 14:35:00 -0600

    Notifications work! Almost, text is wrong

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,59 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const fetch = require('node-fetch');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.sendGroupMessagePushNotification = functions.firestore
+  .document('groups/{groupId}/messages/{messageId}')
+  .onCreate(async (snap, context) => {
+    const groupId = context.params.groupId;
+    // Get the group
+    const groupSnapshot = await admin
+      .firestore()
+      .collection('groups')
+      .doc(groupId)
+      .get();
+    const group = groupSnapshot.data();
+    // Get the member ids
+    const memberIds = Object.keys(group.members);
+    // Convert each ID to a promise that's querying their profile
+    const getMemberProfilesPromises = memberIds.map(userId => {
+      return admin
+        .firestore()
+        .collection('users')
+        .doc(userId)
+        .get();
+    });
+
+    // Fetch the profiles
+    const memberProfileSnapshots = await Promise.all(getMemberProfilesPromises);
+    var messages = [];
+
+    // Loop over all of the profiles and add notification messages to the messages
+    //   batch array.
+    for (let i = 0; i < memberProfileSnapshots.length; i++) {
+      const profile = memberProfileSnapshots[i].data();
+      const userId = memberIds[i];
+      const { pushNotificationsToken } = profile;
+      // Don't do anything if the user has no push notifications
+      if (!pushNotificationsToken) {
+        continue;
+      }
+      // Add a message to the batch
+      messages.push({
+        to: pushNotificationsToken,
+        body: 'You have a new message!',
+      });
+    }
+
+    // Send the EXPO push request
+    return fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(messages),
+    });
+  });
